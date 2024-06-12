ae7e2463b1243bf6b5989e8338ce61af30c95a19
曹潇然 <10607533+cao-xiaoran@user.noreply.gitee.com>
2024-06-12 17:16:58 +0900

    change db engine to mysql

--- aiPlatform/aiPlatform/settings.py
+++ aiPlatform/aiPlatform/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'aiPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AISYS',
+    'USER':'root',
+    'PASSWORD':'PASSWORD',
+    'HOST':'101.37.208.190',
+    'PORT':'3306',
     }
 }
 
