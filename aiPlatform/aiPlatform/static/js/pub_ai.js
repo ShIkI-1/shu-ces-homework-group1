2b5f6c73ce3b0cc2a2d7cbc6521bff5bcc8af38d
kebold <1755482042@qq.com>
2024-08-16 17:36:42 +0800

    发布时给价格

--- aiPlatform/aiPlatform/static/js/pub_ai.js
+++ aiPlatform/aiPlatform/static/js/pub_ai.js
@@ -25,21 +25,27 @@ window.onload = function () {
         config: toolbarConfig,
         mode: 'default', // or 'simple'
     })
-    $("#pub").click(function (event) {
-        event.preventDefault();
-        let title = $("input[name='title']").val();
-        let intro = $("input[name='intro']").val();
-        let flexibility = $("input[name='flexibility']").val();
-        let randomness = $("input[name='randomness']").val();
-        let text = editor.getHtml();
-        $.ajax('/prompt/pub', {
-            method: 'POST',
-            data: {title, intro, flexibility, randomness, text},
-            success: function (result) {
-                if (result['code'] == 200) {
 
-                }
-            }
-        })
-    })
+    $("#pub").click(function (event) {
+    event.preventDefault();
+    let title = $("input[name='title']").val();
+    let intro = $("input[name='intro']").val();
+    let flexibility = $("input[name='flexibility']").val();
+    let randomness = $("input[name='randomness']").val();
+    let price = $("input[name='price']").val();
+    let text = editor.getHtml();
+    $.ajax('/prompt/pub', {
+        method: 'POST',
+        data: { title, intro, flexibility, randomness, price, text },
+        success: function(response) {
+            console.log("Success:", response);
+            // 跳转到 /prompt 页面
+            window.location.href = '/prompt';
+        },
+        error: function(xhr) {
+            console.log("Error:", xhr.responseText);
+        }
+    });
+});
+
 }
